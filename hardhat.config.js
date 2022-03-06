8e38d902832077d7b9ce51efe37a5cb1baeda56e
maikelordaz <maikelordaz@gmail.com>
2022-03-05 22:08:14 -0400

    2nd

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,19 +3,37 @@ require ("@nomiclabs/hardhat-waffle");
 require ("@nomiclabs/hardhat-etherscan");
 require ("hardhat-gas-reporter");
 require ("solidity-coverage");
+require("@openzeppelin/hardhat-upgrades");
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
    solidity: "0.8.4",
-
+/*
   networks: {
+    defaultNetwork: "hardhat",
+    
+    hardhat: {
+      chainId: 1337
+    },
+
     rinkeby: {
       url: process.env.NETWORK_RINKEBY_URL || '',
       accounts: process.env.NETWORK_RINKEBY_PRIVATE_KEY !== undefined 
       ? [process.env.NETWORK_RINKEBY_PRIVATE_KEY] : [],
     },  
+    
   },
+  */
 
   etherscan: {
     apiKey: {
